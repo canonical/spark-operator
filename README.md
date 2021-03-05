c92069c93d3bc88471b67032f46ec5a0794f2fcb
Kenneth Koski <kenneth.koski@canonical.com>
2021-03-04 21:32:47 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Apache Spark Operator
+
+### Overview
+This charm encompasses the Kubernetes Python operator for Apache Spark (see
+[CharmHub](https://charmhub.io/?q=spark)).
+
+The Apache Spark operator is a Python script that wrap the latest released version
+of Apache Spark, providing lifecycle management and handling events such as install,
+upgrade, integrate, and remove.
+
+## Install
+
+To install Apache Spark, run:
+
+    juju deploy spark
+
+For more information, see https://juju.is/docs
---
+++ src/charm.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+
+import logging
+from pathlib import Path
+
+import yaml
+from ops.charm import CharmBase
+from ops.main import main
+from ops.model import ActiveStatus, MaintenanceStatus
+
+from oci_image import OCIImageResource, OCIImageResourceError
+
+log = logging.getLogger()
+
+
+class SparkCharm(CharmBase):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        if not self.model.unit.is_leader():
+            log.info("Not a leader, skipping set_pod_spec")
+            self.model.unit.status = ActiveStatus()
+            return
+
+        self.image = OCIImageResource(self, "oci-image")
+        self.framework.observe(self.on.install, self.set_pod_spec)
+        self.framework.observe(self.on.upgrade_charm, self.set_pod_spec)
+        self.framework.observe(self.on.config_changed, self.set_pod_spec)
+
+    def set_pod_spec(self, event):
+        try:
+            image_details = self.image.fetch()
+        except OCIImageResourceError as e:
+            self.model.unit.status = e.status
+            log.info(e)
+            return
+
+        metrics_enabled = str(bool(self.model.relations["prometheus"])).lower()
+
+        self.model.unit.status = MaintenanceStatus("Setting pod spec")
+        self.model.pod.set_spec(
+            {
+                "version": 3,
+                "serviceAccount": {
+                    "roles": [
+                        {
+                            "global": True,
+                            "rules": [
+                                {
+                                    "apiGroups": [""],
+                                    "resources": ["pods"],
+                                    "verbs": ["*"],
+                                },
+                                {
+                                    "apiGroups": [""],
+                                    "resources": ["configmaps"],
+                                    "verbs": ["*"],
+                                },
+                                {
+                                    "apiGroups": [""],
+                                    "resources": ["services", "secrets"],
+                                    "verbs": ["create", "get", "delete"],
+                                },
+                                {
+                                    "apiGroups": ["extensions"],
+                                    "resources": ["ingresses"],
+                                    "verbs": ["create", "get", "delete"],
+                                },
+                                {
+                                    "apiGroups": [""],
+                                    "resources": ["nodes"],
+                                    "verbs": ["get"],
+                                },
+                                {
+                                    "apiGroups": [""],
+                                    "resources": ["resourcequotas"],
+                                    "verbs": ["get", "list", "watch"],
+                                },
+                                {
+                                    "apiGroups": [""],
+                                    "resources": ["events"],
+                                    "verbs": ["create", "update", "patch"],
+                                },
+                                {
+                                    "apiGroups": ["apiextensions.k8s.io"],
+                                    "resources": ["customresourcedefinitions"],
+                                    "verbs": ["create", "get", "update", "delete"],
+                                },
+                                {
+                                    "apiGroups": ["admissionregistration.k8s.io"],
+                                    "resources": [
+                                        "mutatingwebhookconfigurations",
+                                        "validatingwebhookconfigurations",
+                                    ],
+                                    "verbs": ["create", "get", "update", "delete"],
+                                },
+                                {
+                                    "apiGroups": ["sparkoperator.k8s.io"],
+                                    "resources": [
+                                        "sparkapplications",
+                                        "scheduledsparkapplications",
+                                        "sparkapplications/status",
+                                        "scheduledsparkapplications/status",
+                                    ],
+                                    "verbs": ["*"],
+                                },
+                                {
+                                    "apiGroups": ["scheduling.volcano.sh"],
+                                    "resources": [
+                                        "podgroups",
+                                        "queues",
+                                        "queues/status",
+                                    ],
+                                    "verbs": [
+                                        "get",
+                                        "list",
+                                        "watch",
+                                        "create",
+                                        "delete",
+                                        "update",
+                                    ],
+                                },
+                            ],
+                        }
+                    ],
+                },
+                "containers": [
+                    {
+                        "name": "sparkoperator",
+                        "args": [
+                            "--v=4",
+                            "--logtostderr",
+                            "--namespace",
+                            self.model.name,
+                            f"--enable-metrics={metrics_enabled}",
+                            "--metrics-port",
+                            self.model.config["metrics-port"],
+                            "--metrics-labels=app_type",
+                            "--enable-webhook=true",
+                            "--webhook-svc-namespace",
+                            self.model.name,
+                            "--webhook-port",
+                            self.model.config["webhook-port"],
+                            "--webhook-svc-name",
+                            self.model.app.name,
+                            "--webhook-config-name",
+                            f"{self.model.app.name}-config",
+                            "--webhook-namespace-selector",
+                            f"juju-model={self.model.name}",
+                            "--webhook-fail-on-error=true",
+                        ],
+                        "imageDetails": image_details,
+                        "ports": [
+                            {
+                                "name": "metrics",
+                                "containerPort": int(self.model.config["metrics-port"]),
+                            },
+                            {
+                                "name": "webhook",
+                                "containerPort": int(self.model.config["webhook-port"]),
+                            },
+                        ],
+                        "volumeConfig": [
+                            {
+                                "name": "certs",
+                                "mountPath": "/etc/webhook-certs",
+                                "files": [
+                                    {
+                                        "path": "server-cert.pem",
+                                        "content": Path("/run/cert.pem").read_text(),
+                                    },
+                                    {
+                                        "path": "server-key.pem",
+                                        "content": Path("/run/server.key").read_text(),
+                                    },
+                                    {
+                                        "path": "ca-cert.pem",
+                                        "content": Path("/run/ca.crt").read_text(),
+                                    },
+                                ],
+                            }
+                        ],
+                    }
+                ],
+            },
+            k8s_resources={
+                "kubernetesResources": {
+                    "customResourceDefinitions": [
+                        {"name": crd["metadata"]["name"], "spec": crd["spec"]}
+                        for crd in yaml.safe_load_all(Path("src/crds.yaml").read_text())
+                    ],
+                    "pod": {
+                        "annotations": {
+                            "prometheus.io/scrape": "true",
+                            "prometheus.io/port": self.model.config["metrics-port"],
+                            "prometheus.io/path": "/metrics",
+                        }
+                    },
+                }
+            },
+        )
+        self.model.unit.status = ActiveStatus()
+
+
+if __name__ == "__main__":
+    main(SparkCharm)
