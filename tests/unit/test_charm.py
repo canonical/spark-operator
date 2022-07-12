e9241e887421df6ccb758f410ec5f6e074d9e78e
Natasha Ho <natasha.ho@canonical.com>
2022-07-12 18:27:01 +0800

    update tox and setup unit tests

---
+++ tests/unit/test_charm.py
@@ -0,0 +1,19 @@
+import pytest
+from ops.model import ActiveStatus, WaitingStatus
+from ops.testing import Harness
+
+from charm import SparkCharm
+
+@pytest.fixture
+def harness():
+    return Harness(SparkCharm)
+
+def test_not_leader(harness):
+    harness.begin()
+    assert harness.charm.model.unit.status == WaitingStatus('Waiting for leadership')
+
+def test_successful_install(harness):
+    harness.set_leader(True)
+    harness.begin_with_initial_hooks()
+
+    assert harness.charm.model.unit.status == ActiveStatus()
