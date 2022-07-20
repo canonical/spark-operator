8ced6ceab280577d703a6116f1fb69741b0a9bea
Natasha Ho <natasha.ho@canonical.com>
2022-07-20 18:37:43 -0400

    working version

--- src/charm.py
+++ src/charm.py
@@ -6,20 +6,17 @@ import logging
 from pathlib import Path
 from subprocess import check_call
 
-import yaml
 from charmed_kubeflow_chisme.lightkube.batch import apply_many, delete_many
 from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
 from jinja2 import Environment, FileSystemLoader
 from lightkube import Client, codecs
 from lightkube.core.exceptions import ApiError
-from lightkube.generic_resource import create_global_resource
 from lightkube.models.core_v1 import ServicePort
 from lightkube.resources.admissionregistration_v1 import MutatingWebhookConfiguration
-from lightkube.resources.core_v1 import ConfigMap, Service
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus, MaintenanceStatus, WaitingStatus
+from ops.model import ActiveStatus
 
 log = logging.getLogger()
 
@@ -29,8 +26,6 @@ class SparkCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-
-        self._check_leader()
 
         self._stored.set_default(**self.gen_certs())
         self.lightkube_client = Client(
@@ -42,19 +37,13 @@ class SparkCharm(CharmBase):
         self.service_patcher = KubernetesServicePatch(self, [port])
 
         self.env = Environment(loader=FileSystemLoader("src"))
+        self.env.globals["app_name"] = self.model.app.name
+        self.env.globals["model_name"] = self.model.name
 
         self._mutating_webhook_name = f"{self.model.app.name}-webhook-config"
 
         self.framework.observe(self.on.spark_pebble_ready, self._on_spark_pebble_ready)
         self.framework.observe(self.on.remove, self._on_remove)
-        # self.framework.observe(self.on.install, self.set_pod_spec)
-        # self.framework.observe(self.on.upgrade_charm, self.set_pod_spec)
-        # self.framework.observe(self.on.config_changed, self.set_pod_spec)
-
-    def _check_leader(self):
-        if not self.unit.is_leader():
-            # We can't do anything useful when not the leader, so do nothing.
-            raise CheckFailedError("Waiting for leadership", WaitingStatus)
 
     def _on_spark_pebble_ready(self, event):
         container = event.workload
@@ -102,14 +91,19 @@ class SparkCharm(CharmBase):
                 }
             },
         }
-        container.add_layer("spark", pebble_layer, combine=True)
-        container.autostart()
 
         with open(Path("src/crds.yaml")) as f:
             for obj in codecs.load_all_yaml(
                 f, context={}, create_resources_for_crds=True
             ):
                 self.lightkube_client.apply(obj)
+
+        with open(Path("src/rbac.yaml")) as f:
+            for obj in codecs.load_all_yaml(f, context={}, template_env=self.env):
+                self.lightkube_client.apply(obj)
+
+        container.add_layer("spark", pebble_layer, combine=True)
+        container.autostart()
 
         self.unit.status = ActiveStatus()
 
@@ -120,183 +114,6 @@ class SparkCharm(CharmBase):
             )
         except ApiError as e:
             log.warn(str(e))
-        # TODO: remove crds
-        # TODO: is this needed?
-        for obj in self.lightkube_client.list(
-            resource,
-            labels={"app.juju.is/created-by": f"{self.app_name}"},
-            namespace=self.model.name,
-        ):
-            self.lightkube_client.delete(
-                obj,
-                namespace=self.model.name,
-            )
-
-    def set_pod_spec(self, event):
-        try:
-            image_details = self.image.fetch()
-        except OCIImageResourceError as e:
-            self.model.unit.status = e.status
-            log.info(e)
-            return
-
-        metrics_enabled = str(bool(self.model.relations["prometheus"])).lower()
-        self.model.unit.status = MaintenanceStatus("Setting pod spec")
-        self.model.pod.set_spec(
-            {
-                "version": 3,
-                "serviceAccount": {
-                    "roles": [
-                        {
-                            "global": True,
-                            "rules": [
-                                {
-                                    "apiGroups": [""],
-                                    "resources": ["pods"],
-                                    "verbs": ["*"],
-                                },
-                                {
-                                    "apiGroups": [""],
-                                    "resources": ["configmaps"],
-                                    "verbs": ["*"],
-                                },
-                                {
-                                    "apiGroups": [""],
-                                    "resources": ["services", "secrets"],
-                                    "verbs": ["create", "get", "delete"],
-                                },
-                                {
-                                    "apiGroups": ["extensions"],
-                                    "resources": ["ingresses"],
-                                    "verbs": ["create", "get", "delete"],
-                                },
-                                {
-                                    "apiGroups": [""],
-                                    "resources": ["nodes"],
-                                    "verbs": ["get"],
-                                },
-                                {
-                                    "apiGroups": [""],
-                                    "resources": ["resourcequotas"],
-                                    "verbs": ["get", "list", "watch"],
-                                },
-                                {
-                                    "apiGroups": [""],
-                                    "resources": ["events"],
-                                    "verbs": ["create", "update", "patch"],
-                                },
-                                {
-                                    "apiGroups": ["apiextensions.k8s.io"],
-                                    "resources": ["customresourcedefinitions"],
-                                    "verbs": ["create", "get", "update", "delete"],
-                                },
-                                {
-                                    "apiGroups": ["admissionregistration.k8s.io"],
-                                    "resources": [
-                                        "mutatingwebhookconfigurations",
-                                        "validatingwebhookconfigurations",
-                                    ],
-                                    "verbs": ["create", "get", "update", "delete"],
-                                },
-                                {
-                                    "apiGroups": ["sparkoperator.k8s.io"],
-                                    "resources": [
-                                        "sparkapplications",
-                                        "scheduledsparkapplications",
-                                        "sparkapplications/status",
-                                        "scheduledsparkapplications/status",
-                                    ],
-                                    "verbs": ["*"],
-                                },
-                                {
-                                    "apiGroups": ["scheduling.volcano.sh"],
-                                    "resources": [
-                                        "podgroups",
-                                        "queues",
-                                        "queues/status",
-                                    ],
-                                    "verbs": [
-                                        "get",
-                                        "list",
-                                        "watch",
-                                        "create",
-                                        "delete",
-                                        "update",
-                                    ],
-                                },
-                            ],
-                        }
-                    ],
-                },
-                "containers": [
-                    {
-                        "name": "sparkoperator",
-                        "args": [
-                            "--v=4",
-                            "--logtostderr",
-                            f"--namespace={self.model.name}",
-                            f"--enable-metrics={metrics_enabled}",
-                            f"--metrics-port={self.model.config['metrics-port']}",
-                            "--metrics-labels=app_type",
-                            "--enable-webhook=true",
-                            f"--webhook-svc-namespace={self.model.name}",
-                            f"--webhook-port={self.model.config['webhook-port']}",
-                            f"--webhook-svc-name={self.model.app.name}",
-                            f"--webhook-config-name={self.model.app.name}-config",
-                            f"--webhook-namespace-selector=model.juju.is/name={self.model.name}",
-                            "--webhook-fail-on-error=true",
-                        ],
-                        "imageDetails": image_details,
-                        "ports": [
-                            {
-                                "name": "metrics",
-                                "containerPort": int(self.model.config["metrics-port"]),
-                            },
-                            {
-                                "name": "webhook",
-                                "containerPort": int(self.model.config["webhook-port"]),
-                            },
-                        ],
-                        "volumeConfig": [
-                            {
-                                "name": "certs",
-                                "mountPath": "/etc/webhook-certs",
-                                "files": [
-                                    {
-                                        "path": "server-cert.pem",
-                                        "content": self._stored.cert,
-                                    },
-                                    {
-                                        "path": "server-key.pem",
-                                        "content": self._stored.key,
-                                    },
-                                    {
-                                        "path": "ca-cert.pem",
-                                        "content": self._stored.ca,
-                                    },
-                                ],
-                            }
-                        ],
-                    }
-                ],
-            },
-            k8s_resources={
-                "kubernetesResources": {
-                    "customResourceDefinitions": [
-                        {"name": crd["metadata"]["name"], "spec": crd["spec"]}
-                        for crd in yaml.safe_load_all(Path("src/crds.yaml").read_text())
-                    ],
-                    "pod": {
-                        "annotations": {
-                            "prometheus.io/scrape": "true",
-                            "prometheus.io/port": self.model.config["metrics-port"],
-                            "prometheus.io/path": "/metrics",
-                        }
-                    },
-                }
-            },
-        )
-        self.model.unit.status = ActiveStatus()
 
     def gen_certs(self):
         model = self.model.name
@@ -362,6 +179,8 @@ subjectAltName=@alt_names"""
                 "/run/server.key",
                 "-out",
                 "/run/server.csr",
+                "-subj",
+                f"/CN={app}.{model}.svc",
                 "-config",
                 "/run/ssl.conf",
             ]
@@ -395,17 +214,6 @@ subjectAltName=@alt_names"""
             "key": Path("/run/server.key").read_text(),
             "ca": Path("/run/ca.crt").read_text(),
         }
-
-
-class CheckFailedError(Exception):
-    """Raise this exception if one of the checks in main fails."""
-
-    def __init__(self, msg, status_type=None):
-        super().__init__()
-
-        self.msg = str(msg)
-        self.status_type = status_type
-        self.status = status_type(msg)
 
 
 if __name__ == "__main__":
