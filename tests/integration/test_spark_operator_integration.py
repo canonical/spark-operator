a6cd49a1d5e8d083f09b7ebba4cb998128caf7bb
Natasha Ho <natasha.ho@canonical.com>
2022-07-21 13:31:46 -0400

    fix integration tests

--- tests/integration/test_spark_operator_integration.py
+++ tests/integration/test_spark_operator_integration.py
@@ -2,6 +2,7 @@
 # See LICENSE file for licensing details.
 
 import logging
+import time
 
 import pytest
 from lightkube.resources.core_v1 import Pod
@@ -22,12 +23,19 @@ async def test_build_and_deploy(ops_test, helpers):
         application_name=spark_app_name,
         trust=True,
     )
-    await ops_test.model.wait_for_idle(status="active", raise_on_blocked=True, timeout=300)
+    await ops_test.model.wait_for_idle(
+        [spark_app_name], status="active", raise_on_blocked=True, timeout=300
+    )
+
+    # juju bug: reports app as active before pods are actually ready
+    # https://bugs.launchpad.net/juju/+bug/1981833
+    time.sleep(60)
+
     assert ops_test.model.applications[spark_app_name].units[0].workload_status == "active"
 
 
 @retry(
-    wait=wait_exponential(multiplier=1, min=1, max=10),
+    wait=wait_exponential(multiplier=3, min=1, max=10),
     stop=stop_after_delay(60),
     reraise=True,
     before=before_log(log, logging.INFO),
